c0e50cd9ede18f0390124c92f2f36ed14b2f10c0
khaledolemy <khaledolemy@gmail.com>
2024-02-21 18:21:31 +0200

    delete_&_all_(cls)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            out = {}
+            for obj, val in FileStorage.__objects.items():
+                if f"{cls}." not in obj:
+                    out[obj] = val
+            return out
+        else:
+            return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +52,13 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """delete instance (obj)"""
+        if obj:
+            to_delete = "{}.{}".format(obj.to_dict()['__class__'], obj.id)
+            if to_delete in self.__objects:
+                del self.__objects[to_delete]
