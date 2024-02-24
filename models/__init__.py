fa8cb17dd243a38713d76711d5b3288990f22b0f
khaledolemy <khaledolemy@gmail.com>
2024-02-24 12:39:00 +0200

    restore

--- models/__init__.py
+++ models/__init__.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-from models.engine.db_storage import DBStorage
-from models.base_model import BaseModel, Base
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
-from models.user import User
-import os
 
-if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-    storage = DBStorage()
-else:
-    storage = FileStorage()
+
+storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,29 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models import storage
+from models.base_model import BaseModel
 
 
-class State(BaseModel, Base):
-    """
-    State class -> table `states`
-    cols;   name: string(128), not nullable
-
-    cascade relation to citites.
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete, delete-orphan",
-                           backref="state")
-
-    @property
-    def cities(self):
-        all_cities = storage.all()
-        out_list = []
-        for inp in all_cities:
-            if inp.split('.')[0] == "City":
-                if all_cities[inp].state_id == self.id:
-                    out_list.append(all_cities[inp])
-        return out_list
+class State(BaseModel):
+    """ State class """
+    name = ""
