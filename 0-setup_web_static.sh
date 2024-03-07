342c6c74504a7bc779688316d2e45231016df0fd
Yasmineltair <yasmineltair293@gmail.com>
2024-03-07 15:45:20 -0800

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,23 @@
 #!/usr/bin/env bash
 # Bash script that sets up your web servers for the deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Simple Test" | sudo tee /data/web_static/releases/test/index.html
+if ! dpkg -l | grep -qw nginx; then
+        sudo apt-get update
+	sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo "<html>
+<head>
+</head>
+<body>
+i wish finish this task
+</body>
+>/html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i "38i \\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n" /etc/nginx/sites-available/default
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-	index index.html index.htm;
-    }
-
-    location /redirect_me {
-	return 301 http://cuberule.com/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-	root /var/www/html;
-	internal;
-    }
-}" > /etc/nginx/sites-available/default
-sudo service nginx start
+sudo service nginx restart
