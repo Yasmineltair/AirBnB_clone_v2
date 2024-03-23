0d9fb66e5e5163f7ae0ae0a312dd1febd2e387e7
Yasmineltair <yasmineltair293@gmail.com>
2024-03-23 15:36:10 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" a script that starts a Flask web application:"""
+from flask import Flask
+
+
+web_app = Flask(__name__)
+
+
+@web_app.route("/", strict_slashes=False)
+def homepage():
+    """ home page"""
+    return "Hello HBNB!"
+
+
+@web_app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ hbnb"""
+    return "HBNB"
+
+
+@web_app.route("/c/<text>", strict_slashes=False)
+def c_fun(text):
+    """ display “C ”, followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
+
+
+@web_app.route("/python/<text>", strict_slashes=False)
+def py(text="is cool"):
+    """ display “Python ”, followed by the value of the text"""
+    return "Python {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    web_app.run(host="0.0.0.0", port=5000)
