b47a9f2ea2e81993153aba5b69ce5896382da533
Yasmineltair <yasmineltair293@gmail.com>
2024-03-24 11:08:44 -0700

    base

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -67,7 +67,3 @@ class FileStorage:
             if keyName in FileStorage.__objects:
                 del (FileStorage.__objects[keyName])
                 self.save()
-
-    def close(self):
-        """ method for deserializing the JSON file to objects"""
-        self.reload()
